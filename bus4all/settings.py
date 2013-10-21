255b0a05d816ab33861837b840f7e9f7c7970931
Bus4All <bus4all.recife@gmail.com>
2013-10-21 17:36:45 -0300

    Initial scrapy commit

---
+++ bus4all/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for bus4all project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'bus4all'
+
+SPIDER_MODULES = ['bus4all.spiders']
+NEWSPIDER_MODULE = 'bus4all.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'bus4all (+http://www.yourdomain.com)'
